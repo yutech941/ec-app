daf125f85693808e37c0734689b296678d1f1bdb
小林優一 <y.k94cofitec179@gmail.com>
2021-08-15 20:10:23 +0900

    initialStateとreducersを記述した。

---
+++ src/reducks/store/initialState.js
@@ -0,0 +1,12 @@
+const initialState ={
+    products:{
+    },
+    users:{
+        icon:"",
+        isSignedIn:false,
+        uid:"",
+        username:""
+    }
+};
+
+export default initialState
