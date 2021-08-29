043d3e0b6dd8ca6a4cb52dbfef4565ed254c8808
小林優一 <y.k94cofitec179@gmail.com>
2021-08-29 20:26:47 +0900

    Routerなど追加

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import createStore from './reducks/store/store'
+import {ConnectedRouter} from 'connected-react-router'
+import * as History from 'history'
 import './index.css';
 import App from './App';
 
-export const store = createStore();
+const history = History.createBrowserHistory();
+export const store = createStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
+    <ConnectedRouter history={history}>
     <App/>
+    </ConnectedRouter>
     </Provider>,
 document.getElementById('root')
 );
