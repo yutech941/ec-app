2a7fd72d15e3dce2270aaa3eb32a9e0debac26c8
小林優一 <y.k94cofitec179@gmail.com>
2021-08-31 10:26:35 +0900

    ログインボタンを設置した。

--- src/template/Login.jsx
+++ src/template/Login.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import {useDispatch} from 'react-redux'
+import {useDispatch,useSelector} from 'react-redux'
 import {push} from 'connected-react-router'
 
 const Login = () => {
     const dispatch = useDispatch()
+    const selector = useSelector(state => state);
+
+    console.log(selector.router);
 
     return(
         <div>
             <h2>ログイン</h2>
             <button onClick={() => dispatch(push('/'))}>
-
+            ログインする
             </button>
         </div>
     )
