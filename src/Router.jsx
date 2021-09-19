c13adde180dda68e5ba19238bb63e9b2a140d85a
小林優一 <y.k94cofitec179@gmail.com>
2021-09-19 17:17:32 +0900

    ファイル名変更

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import {Switch,Route} from 'react-router';
-import {Login,Home } from './template';
+import React from "react";
+import { Switch, Route } from "react-router";
+import { Home } from "./templates";
+import { LoginContainer } from "./containers";
 
 const Router = () => {
-    return(
-        <Switch>
-            <Route exact path={"/login"} component={Login} />
-            <Route exact path={"(/)?"} component={Home}/>
-        </Switch>
-    )
-}
-export default Router
+  return (
+    <Switch>
+      <Route exact path={"/login"} component={LoginContainer} />
+      {/* <Route exact path={"/login"} component={Login} /> */}
+      <Route exact path={"(/)?"} component={Home} />
+    </Switch>
+  );
+};
+export default Router;
--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,7 +1,7 @@
 import { signInAction } from "./actions";
 import { push } from "connected-react-router";
 
-export const siginIn = () => {
+export const signIn = () => {
   return async (dispatch, getState) => {
     const state = getState();
     const isSignedIn = state.users.isSignedIn;
