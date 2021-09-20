d8a039990a29a08f21f6d5b9630116a4379eef12
小林優一 <y.k94cofitec179@gmail.com>
2021-09-20 15:23:14 +0900

    firebaseのConfigファイルを作成した。

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5SBXHC9tgks-Yhex4oD5aumKqL_8JOSA",
+  authDomain: "ec-app-b0da7.firebaseapp.com",
+  projectId: "ec-app-b0da7",
+  storageBucket: "ec-app-b0da7.appspot.com",
+  messagingSenderId: "251842645114",
+  appId: "1:251842645114:web:a0e3f8f73dca2826237fc3",
+  measurementId: "G-13FWT8X0CY",
+};
