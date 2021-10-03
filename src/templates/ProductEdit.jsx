9ebd0ed10c634302d252dc0aef91d09912f23bcc
小林優一 <y.k94cofitec179@gmail.com>
2021-10-03 15:17:04 +0900

    商品編集画面を作成した。

---
+++ src/templates/ProductEdit.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useCallback } from "react";
+import { TextInput } from "../components/UIkit";
+
+const ProductEdit = () => {
+  const [name, setName] = useState(""),
+    [description, setDescription] = useState(""),
+    [category, setCategory] = useState(""),
+    [gender, setGender] = useState(""),
+    [price, setPrice] = useState("");
+
+  const inputName = useCallback(
+    (event) => {
+      setName(event.target.value);
+    },
+    [setName]
+  );
+
+  const inputDescription = useCallback(
+    (event) => {
+      setDescription(event.target.value);
+    },
+    [setDescription]
+  );
+
+  const inputPrice = useCallback(
+    (event) => {
+      setPrice(event.target.value);
+    },
+    [setPrice]
+  );
+
+  return (
+    <section>
+      <h2 className={"u-text__headline u-text-center"}>商品の登録・編集</h2>
+      <div className={"c-section-container"}>
+        <TextInput
+          fullWidth={true}
+          label={"商品名"}
+          multiline={false}
+          required={true}
+          onChange={inputName}
+          rows={1}
+          value={name}
+          type={"text"}
+        />
+        <TextInput
+          fullWidth={true}
+          label={"商品説明"}
+          multiline={true}
+          required={true}
+          onChange={inputDescription}
+          rows={5}
+          value={description}
+          type={"text"}
+        />
+        <TextInput
+          fullWidth={true}
+          label={"価格"}
+          multiline={false}
+          required={true}
+          onChange={inputPrice}
+          rows={1}
+          value={price}
+          type={"number"}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ProductEdit;
