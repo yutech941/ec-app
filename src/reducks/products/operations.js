f8eeefc2122c16aa858dc09b1f74734c81774bdd
小林優一 <y.k94cofitec179@gmail.com>
2021-10-05 20:55:55 +0900

    productsに必要なファイルを追加した。

---
+++ src/reducks/products/operations.js
@@ -0,0 +1,144 @@
+import { signInAction, signOutAction } from "./actions";
+import { push } from "connected-react-router";
+import { auth, db, FirebaseTimestamp } from "../../firebase/index";
+
+export const listenAuthState = () => {
+  return async (dispatch) => {
+    return auth.onAuthStateChanged((user) => {
+      if (user) {
+        const uid = user.uid;
+
+        db.collection("users")
+          .doc(uid)
+          .get()
+          .then((snapshot) => {
+            const data = snapshot.data();
+
+            dispatch(
+              signInAction({
+                isSignedIn: true,
+                role: data.role,
+                uid: uid,
+                username: data.username,
+              })
+            );
+          });
+      } else {
+        dispatch(push("/signin"));
+      }
+    });
+  };
+};
+
+export const resetPassword = (email) => {
+  return async (dispatch) => {
+    if (email === "") {
+      alert("必須項目が未入力です");
+      return false;
+    } else {
+      auth
+        .sendPasswordResetEmail(email)
+        .then(() => {
+          alert(
+            "入力されたアドレスにパスワードリセット用のメールをお送りしました。"
+          );
+          dispatch(push("/signin"));
+        })
+        .catch(() => {
+          alert(
+            "パスワードリセットに失敗しました。通信の良いところで再度試してください。"
+          );
+        });
+    }
+  };
+};
+
+export const signIn = (email, password) => {
+  return async (dispatch) => {
+    //Validation
+
+    if (email === "" || password === "") {
+      alert("必須項目が未入力です");
+      return false;
+    }
+    auth.signInWithEmailAndPassword(email, password).then((result) => {
+      const user = result.user;
+
+      if (user) {
+        const uid = user.uid;
+
+        db.collection("users")
+          .doc(uid)
+          .get()
+          .then((snapshot) => {
+            const data = snapshot.data();
+
+            dispatch(
+              signInAction({
+                isSignedIn: true,
+                role: data.role,
+                uid: uid,
+                username: data.username,
+              })
+            );
+            dispatch(push("/"));
+          });
+      }
+    });
+  };
+};
+
+export const signUp = (username, email, password, confirmPassword) => {
+  return async (dispatch) => {
+    //Validation
+    if (
+      username === "" ||
+      email === "" ||
+      password === "" ||
+      confirmPassword === ""
+    ) {
+      alert("必須項目が未入力です");
+      return false;
+    }
+    if (password !== confirmPassword) {
+      alert("パスワードが一致しません。もう一度お試ししてください。");
+      return false;
+    }
+
+    return auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
+
+        if (user) {
+          const uid = user.uid;
+          const timestamp = FirebaseTimestamp.now();
+
+          const userInitialDate = {
+            created_at: timestamp,
+            email: email,
+            role: "customer",
+            uid: uid,
+            update_at: timestamp,
+            username: username,
+          };
+
+          db.collection("users")
+            .doc(uid)
+            .set(userInitialDate)
+            .then(() => {
+              dispatch(push("/"));
+            });
+        }
+      });
+  };
+};
+
+export const signOut = () => {
+  return async (dispatch) => {
+    auth.signOut().then(() => {
+      dispatch(signOutAction());
+      dispatch(push("/signin"));
+    });
+  };
+};
