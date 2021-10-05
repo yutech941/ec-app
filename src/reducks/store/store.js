c9b98268481c30dd57baeaf1223f0e3a094bf682
小林優一 <y.k94cofitec179@gmail.com>
2021-10-05 20:56:59 +0900

    productsのstate情報を保存した。

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -3,18 +3,25 @@ import {
   combineReducers,
   applyMiddleware,
 } from "redux";
-
+import { createLogger } from "redux-logger/src";
 import { connectRouter, routerMiddleware } from "connected-react-router";
 import thunk from "redux-thunk";
 
+import { ProductsReducer } from "../products/reducers";
 import { UsersReducer } from "../users/reducers";
 
 export default function createStore(history) {
+  const logger = createLogger({
+    collaped: true,
+    diff: true,
+  });
+
   return reduxCreateStore(
     combineReducers({
+      products: ProductsReducer,
       router: connectRouter(history),
       users: UsersReducer,
     }),
-    applyMiddleware(routerMiddleware(history), thunk)
+    applyMiddleware(logger, routerMiddleware(history), thunk)
   );
 }
