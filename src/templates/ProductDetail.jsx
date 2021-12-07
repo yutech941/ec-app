881b24511699deb7f49b68c9dd456b1fa153864d
小林優一 <y.k94cofitec179@gmail.com>
2021-12-07 22:15:10 +0900

    ProductDetailを作成した。

---
+++ src/templates/ProductDetail.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
+import { db } from "../firebase/index";
+import { makeStyles } from "@material-ui/styles";
+import HTMLReactParser from "html-react-parser";
+
+const useStyles = makeStyles((theme) => ({
+  sliderBox: {
+    [theme.breakpoints.down("sm")]: {
+      margin: "0 auto 24px auto",
+      height: 320,
+      width: 320,
+    },
+    [theme.breakpoints.up("sm")]: {
+      margin: "0 auto",
+      height: 400,
+      width: 400,
+    },
+  },
+  detail: {
+    [theme.breakpoints.down("sm")]: {
+      margin: "0 auto 24px auto",
+      height: "auto",
+      width: 320,
+    },
+    [theme.breakpoints.up("sm")]: {
+      margin: "0 auto",
+      height: "auto",
+      width: 400,
+    },
+  },
+  price: {
+    fontSize: 36,
+  },
+}));
+
+const returnCodeToBr = (text) => {
+  if (text === "") {
+    return text;
+  } else {
+    return HTMLReactParser(text.replace(/\r?\n/g, "<br/>"));
+  }
+};
+
+const ProductDetail = () => {
+  const classes = useStyles();
+  const selector = useSelector((state) => state);
+  const path = selector.router.location.pathname;
+  const id = path.split("/product/");
+
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    db.collection("products")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        const data = doc.data();
+        setProduct(data);
+      });
+  }, []);
+
+  return (
+    <section className="c-section-wrapin">
+      {product && (
+        <div className="p-grid__row">
+          <div className={classes.sliderBox}></div>
+          <div className={classes.detail}>
+            <h2 className={"u-text__headline"}>{product.name}</h2>
+            <p className={classes.price}>{product.price.toLocaleString()}</p>
+            <div className={"module-spacer--small"} />
+            <div className={"module-spacer--small"} />
+            <p>{returnCodeToBr(product.descirption)}</p>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default ProductDetail;
